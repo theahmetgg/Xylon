80f01f847f8ce413ee8c19242676c95d8e02eeaf
theahmetgg <ahmet1998gulmez@gmail.com>
2024-10-09 23:32:44 +0300

    add elements to header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Header from "./sections/Header";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="overflow-hidden">
+      <Header />
+    </main>
+  );
 };
 
 export default App;
