9919d80da6b3863966deae92dd5f3810acd8a31f
theahmetgg <ahmet1998gulmez@gmail.com>
2024-10-18 15:40:37 +0300

    Header bug fixed

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,20 +1,47 @@
-import clsx from "clsx";
-import { useState } from "react";
 import { Link as LinkScroll } from "react-scroll";
-
-const NavLink = ({ title }) => (
-  <LinkScroll className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
-    {title}
-  </LinkScroll>
-);
+import { useEffect, useState } from "react";
+import clsx from "clsx";
 
 const Header = () => {
+  const [hasScrolled, setHasScrolled] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 32);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const NavLink = ({ title }) => (
+    <LinkScroll
+      onClick={() => setIsOpen(false)}
+      to={title}
+      offset={-100}
+      spy
+      smooth
+      activeClass="nav-active"
+      className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5"
+    >
+      {title}
+    </LinkScroll>
+  );
+
   return (
-    <header className="fixed top-0 left-0 z-50 w-full py-10">
+    <header
+      className={clsx(
+        "fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4",
+        hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px]"
+      )}
+    >
       <div className="container flex h-14 items-center max-lg:px-5">
         <a className="lg:hidden flex-1 cursor-pointer z-2">
-          <img src="/images/xora.svg" alt="logo" width={115} height={55} />
+          <img src="/images/xora.svg" width={115} height={55} alt="logo" />
         </a>
 
         <div
@@ -23,7 +50,7 @@ const Header = () => {
             isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none"
           )}
         >
-          <div className="w-full max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
+          <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
             <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
               <ul className="flex max-lg:block max-lg:px-12">
                 <li className="nav-li">
@@ -31,10 +58,11 @@ const Header = () => {
                   <div className="dot" />
                   <NavLink title="pricing" />
                 </li>
+
                 <li className="nav-logo">
                   <LinkScroll
                     to="hero"
-                    offset={-100}
+                    offset={-250}
                     spy
                     smooth
                     className={clsx(
@@ -43,12 +71,13 @@ const Header = () => {
                   >
                     <img
                       src="/images/xora.svg"
-                      alt="logo"
                       width={160}
                       height={55}
+                      alt="logo"
                     />
                   </LinkScroll>
                 </li>
+
                 <li className="nav-li">
                   <NavLink title="faq" />
                   <div className="dot" />
@@ -66,7 +95,7 @@ const Header = () => {
                 className="relative z-2"
               />
               <img
-                src="/images/bg-outlines-fill.svg"
+                src="/images/bg-outlines-fill.png"
                 width={960}
                 height={380}
                 alt="outline"
@@ -75,6 +104,7 @@ const Header = () => {
             </div>
           </div>
         </div>
+
         <button
           className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center"
           onClick={() => setIsOpen((prevState) => !prevState)}
