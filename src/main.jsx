3e907797189270f3e490333f4806604c92db2f15
theahmetgg <ahmet1998gulmez@gmail.com>
2024-10-25 21:24:48 +0300

    main.jsx updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
+import ReactDOM from "react-dom";
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
+  document.getElementById("root")
 );
